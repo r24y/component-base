f0a06f75d1912fe2085fae498b82c4356084b781
Ryan A. Muller <ryan@muller.io>
2015-07-01 14:10:20 -0400

    Fix .npmignore error

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
 
 gulp.task('default', function () {
-    return gulp.src('src/index.js')
+    return gulp.src('src/main.js')
+        .pipe(rename('index.js'))
         .pipe(babel())
         .pipe(browserify())
         .pipe(gulp.dest('.'));
